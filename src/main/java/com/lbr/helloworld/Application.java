733e3bd3a21f1488b117554cfeb715fe28878c8a
luoboran <boran.luo@hunteron.com>
2018-04-24 10:18:32 +0800

    first commit

---
+++ src/main/java/com/lbr/helloworld/Application.java
@@ -0,0 +1,18 @@
+/**
+ * <p>Title: Application.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2016</p>
+ * <p>Company: www.zto.com</p>
+ */
+package com.lbr.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ src/main/java/com/lbr/helloworld/HomeController.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * <p>Title: HomeController.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2016</p>
+ * <p>Company: www.zto.com</p>
+ */
+package com.lbr.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "SUCCESS";
+    }
+}
