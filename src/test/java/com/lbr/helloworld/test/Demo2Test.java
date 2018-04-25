f9f2f60c8071a779357c048e919f3a6471da0b22
luoboran <boran.luo@hunteron.com>
2018-04-25 11:47:47 +0800

    添加插件生成 maven+junit的测试报告

---
+++ src/test/java/com/lbr/helloworld/test/Demo2Test.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * <p>Title: Demo2Test.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2016</p>
+ * <p>Company: www.zto.com</p>
+ */
+package com.lbr.helloworld.test;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class Demo2Test {
+
+    @Test
+    public void TestMth3() {
+        assertTrue("msg : mth3 test test test test", true);
+    }
+
+    @Test
+    public void TestMth4() {
+        assertTrue("msg : mth4 test test test test", false);
+    }
+
+}
---
+++ src/test/java/com/lbr/helloworld/test/DemoTest.java
@@ -0,0 +1,36 @@
+/**
+ * <p>Title: DemoTest.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2016</p>
+ * <p>Company: www.zto.com</p>
+ */
+package com.lbr.helloworld.test;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class DemoTest {
+
+    @Test
+    public void TestMth1() {
+        assertTrue("msg : mth1 test test test test", true);
+    }
+
+    @Test
+    public void TestMth2() {
+        assertTrue("msg : mth2 test test test test", true);
+    }
+
+    @Test
+    public void TestMth3() {
+        assertTrue("msg : false false false false false", false);
+    }
+
+    @Test
+    public void TestMth4() {
+        assertTrue("msg : false false false false false", false);
+    }
+
+
+}
